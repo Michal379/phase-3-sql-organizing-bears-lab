7e6b51554900c6ae7a6b0a1486713866ec6a6d78
Michal <khakaimic4@gmail.com>
2023-05-26 03:12:48 +0300

    completed lab

---
+++ lib/insert.sql
@@ -0,0 +1,15 @@
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", 23,"M", "Black", "quiet", "No" );
+
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ("Rowdy", 63,"M", "White", "loud", "No" );
+
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ("Tabitha", 18,"F", "Fair", "relaxed", "Yes" );
+
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown", 40,"M", "Light", "peaceful", "Yes" );
+
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ("Melissa", 28,"F", "White", "social", "Yes" );
+
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ("Grinch", 19,"M", "Medium", "optimistic", "Yes" );
+
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ("Wendy", 45,"F", "Black", "analytical", "No" );
+
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES (NULL, 32,"F", "Fair", "chill", "No" );
